12fe169e7a2e48eae07bf80329b9018dcc7bbb2c
nick <mbiletskiy@artelogic.net>
2020-03-15 01:59:59 +0200

    first commit]

---
+++ list_comprehensions.py
@@ -0,0 +1,20 @@
+
+
+mylist = [x**2 for x in range(10)]
+
+print(mylist)
+
+
+oo = 3
+def test():
+    if oo != 5:
+        print('not')
+    if oo != 3:
+        print("1234")
+    if oo != 4:
+        print('ee22ee')
+    else:
+        print('2')
+
+
+test()
