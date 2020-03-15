ae3746df8bea06d33445948062ab041306c8530c
nick <mbiletskiy@artelogic.net>
2020-03-15 14:32:15 +0200

    last

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -8,13 +8,13 @@ print(mylist)
 oo = 3
 def test():
     if oo != 5:
-        print('11111')
+        print('11111finally')
     if oo != 3:
-        print("22222")
+        print("2222211111finally")
     if oo != 4:
-        print('33333')
+        print('3333311111finally')
     else:
-        print('444444')
+        print('44444411111finally')
 
 
 test()
