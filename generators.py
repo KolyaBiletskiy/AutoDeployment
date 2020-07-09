1d060ac6631e44b3f405e42e848b3535c6953e00
nick <mbiletskiy@artelogic.net>
2020-07-09 23:08:29 +0300

    update

--- generators.py
+++ generators.py
@@ -41,10 +41,64 @@ print(32%2)
 
 
 
+print('-------------------------')
+
+
+def my_gen(n):
+    for i in range(n):
+        yield i**2
+
+
+gen_object_square = my_gen(5)
+
+for num in gen_object_square:
+    print(num)
+
+arra = ['1', 2, 3, 4, 5, 5, 6, 6]
+
+arr_map = map(str, arra)
+
+for i in arr_map:
+    print(type(i))
+
+
+nla = lambda x, y, z: 100 + x + 1 + y + z
+
+print(nla(10, 20, 30))
+
+
+def outer(x):
+    lam = lambda y: x + y
+    return lam(5)
+
+
+print(outer(5))
 
 
 
+test_l = ['aa', 'bb', 'ab']
+
+for item in test_l:
+    if item[0] == 'a':
+        print(item)
+
+
+print([w for w in test_l if w[0] == 'a'])
+
+
+class Base:
+    def a(self):
+        print("Base_a")
+
+class Inherited(Base):
+    def a(self):
+        # Base.a(self)
+        # call base method here
+        print("Inherited_a")
 
 
 
+obj = Inherited()
 
+# obj.a()
+Base.a(obj.a())
